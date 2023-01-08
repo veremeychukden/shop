59c1bf68a9bf63f71b95c84b515df0ebd48aedf7
veremeychukden <veremeychukden@gmail.com>
2023-01-08 13:26:20 +0200

    updating home page with category menu component and category item item component

---
+++ src/components/category-menu/category-menu.js
@@ -0,0 +1,15 @@
+import CategoryItem from '../category-item/category-item';
+import './category-menu.scss';
+
+const CategoryMenu = ({ categories }) => {
+  return(
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))} 
+    </div>
+  );
+
+}
+
+export default CategoryMenu;
